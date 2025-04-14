0528ba0b2264f324375d997ea4b791ac4af9b749
ManuelLeal310 <manel.leal96@gmail.com>
2025-04-14 14:08:07 +0100

    logout

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,19 +5,22 @@ import axios from "axios";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
   const nav = useNavigate();
 
   function handleLogin(event) {
     event.preventDefault();
-    const adminToCreateInDB = { email, password };
+    const adminToLogin = { email, password };
     axios
       .post(`${import.meta.env.VITE_API_URL}/admin/login`, adminToLogin)
       .then((res) => {
         console.log("admin was logged in", res);
-        nav("/profile");
+        localStorage.setItem("adminToken", res.data.authToken);
+        nav("/admin");
       })
       .catch((err) => {
         console.log(err);
+        setErrorMessage(err.response.data.errorMessage);
       });
   }
 
@@ -49,6 +52,7 @@ function Login() {
         </label>
         <button>Login</button>
       </form>
+      {errorMessage ? <p>{errorMessage}</p> : null}
       <p>
         New here...<Link to="/">Sign up</Link>
       </p>
